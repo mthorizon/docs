9814952e4c32e5cd80c39786bbc56175ddd3ca7d
JamesLawton <crossoveranx@gmail.com>
2024-03-04 16:00:27 +0000

    Build: (f143944) Merge pull request #3 from 0xsequence/GH-actions
    Create build.yml

---
+++ assets/wallets-CTU13-lg.js
@@ -0,0 +1,5 @@
+import{u as r,j as e}from"./index-DRwrJhDD.js";const o={title:"Wallets",description:"undefined"};function s(n){const t={a:"a",div:"div",h1:"h1",header:"header",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"wallets",children:["Wallets",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#wallets",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(t.p,{children:"..."}),`
+`,e.jsx(t.p,{children:"lots of wallets out there.."}),`
+`,e.jsx(t.p,{children:"as overview,.. EOA.. smart wallets.."}),`
+`,e.jsx(t.p,{children:"lets do some Comparison .. etc.."})]})}function i(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s(n)}export{i as default,o as frontmatter};
