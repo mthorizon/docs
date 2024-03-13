a99e3f9f172115f75acea2d6e475c2079e899d77
JamesLawton <crossoveranx@gmail.com>
2024-03-13 11:38:53 +0000

    Build: (ae2398c) Merge pull request #9 from 0xsequence/guidesMigration
    Guides migration

--- assets/01-overview-B4xcSbEg.js
+++
@@ -1,7 +0,0 @@
-import{u as l,j as e}from"./index-Bd1BbtIx.js";const r={slug:"/wallet",title:"Wallet",description:"undefined"};function a(n){const t={a:"a",div:"div",h1:"h1",header:"header",p:"p",...l(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"wallet",children:["Wallet",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#wallet",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
-`,e.jsx(t.p,{children:"Sequence Wallet gives you a friendly, non-custodial wallet for your users with multi-chain support and seamless integration."}),`
-`,e.jsx("div",{class:"text--center",children:e.jsx("img",{src:"/img/wallet-screen.png"})}),`
-`,e.jsx(t.p,{children:`Most wallets for Ethereum have a single private key that has full control over a wallet, i.e. an Externally Owned Account (EOAs).
-However, a Sequence wallet is a Smart Contract that is deployed on the blockchain and which can be controlled by multiple private keys.
-These keys are meant to be controlled by the wallet owner and provide added security since more than one of these keys would need to be
-compromised for the wallet to be compromised.`})]})}function s(n={}){const{wrapper:t}={...l(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(a,{...n})}):a(n)}export{s as default,r as frontmatter};
