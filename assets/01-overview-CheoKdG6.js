b8707763a4c6802aeb8c01683ab4534f4aa696bd
JamesLawton <crossoveranx@gmail.com>
2024-04-11 18:03:05 +0000

    Build: (6913fa8) updated API links (#46)

---
+++ assets/01-overview-CheoKdG6.js
@@ -0,0 +1,17 @@
+import{d as s,j as e}from"./index-CQ1xESna.js";const o={title:"SequenceKit SDK",description:"undefined"};function i(t){const n={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"sequencekit-sdk",children:["SequenceKit SDK",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#sequencekit-sdk",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["Sequence Kit 🧰 is the ultimate toolkit for effortlessly integrating web3 wallets into your applications, providing your users with a smooth and secure onboarding experience. With our robust tools built on the popular ",e.jsx(n.a,{href:"https://wagmi.sh/",children:"wagmi"})," library, unlock a realm of possibilities in the world of web3."]}),`
+`,e.jsxs(n.h2,{id:"key-features",children:["Key Features",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#key-features",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Universal Connections"}),": Seamlessly connect via popular social logins such as Facebook, Google, Discord, and more! Your users will enjoy a smooth, secure onboarding process. 🔐🪪"]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Web3 Wallet Integration"}),": Effortlessly integrate with leading web3 wallets like WalletConnect and MetaMask. Unleash the power of blockchain with just a few clicks! 🦊 ⛓️"]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Embedded Wallet Experience"}),": Provide your users with a comprehensive embedded wallet, enabling them to manage their coins and collectibles all within your own application. 👛 🖼️ 🪙"]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["Explore the potential of Sequence Kit by trying out our ",e.jsx(n.a,{href:"https://0xsequence.github.io/kit",children:"demo"}),"!"]}),`
+`,e.jsx(n.header,{children:e.jsxs(n.h1,{id:"next-steps",children:["Next Steps",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#next-steps",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["Ready to integrate Sequence Kit into your application? Check out our ",e.jsx(n.a,{href:"#TODO",children:"Getting Started guide"}),". If you're already familiar with Sequence Kit dive into the available ",e.jsx(n.a,{href:"#TODO",children:"configuration options"})," or take a look at the ",e.jsx(n.a,{href:"#TODO",children:"checkout"}),"."]})]})}function a(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{a as default,o as frontmatter};
