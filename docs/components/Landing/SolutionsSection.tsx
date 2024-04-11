3d51ed292316398cb2ffbd93466d26d52755950e
Morgan Moskalyk <morgan.moskalyk@protonmail.ch>
2024-04-11 10:36:57 -0400

    removed line on landing page (#42)

--- docs/components/Landing/SolutionsSection.tsx
+++ docs/components/Landing/SolutionsSection.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import * as ICONS from './icons'
 
 export const SolutionsSection = () => (
-  <section className="flex flex-col gap-5 border-t dark:border-white-10 border-black-10">
+  <section className="flex flex-col gap-5 border-t border-black-10">
     <div className="flex flex-col gap-1 py-5">
       <h3 className="text-3xl text-themed-primary font-bold">Solutions</h3>
       <p className="text-md leading-6 font-medium max-w-xl dark:text-white-80 text-black-80">
