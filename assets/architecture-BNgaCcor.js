0f6d83c28318c4625fdacd7422ad6e40a72425b5
JamesLawton <crossoveranx@gmail.com>
2024-04-16 08:11:09 +0000

    Build: (a3fd1ba) Lootbox guide (#66)
    * add work in progress guide
    
    * added updated demo link
    
    * updated links and guide

---
+++ assets/architecture-BNgaCcor.js
@@ -0,0 +1,38 @@
+import{d as i,j as e}from"./index-BG6_g6N7.js";const r={title:"Overview",description:"undefined"};function n(a){const t={a:"a",div:"div",em:"em",h1:"h1",h2:"h2",header:"header",li:"li",ol:"ol",p:"p",strong:"strong",...i(),...a.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"overview",children:["Overview",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(t.h2,{id:"embedded-wallets-architecture",children:["Embedded Wallets Architecture",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#embedded-wallets-architecture",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(t.p,{children:"Sequence Embedded Wallets are native smart contract multisigs, the configuration of the multisig can be seen as a 2/2, both of which are required to sign in order to operate them; each one of the signers protects the wallet from a different facet of possible threats."}),`
+`,e.jsxs(t.h2,{id:"signer-1-sequence-auth",children:["Signer 1: Sequence Auth",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#signer-1-sequence-auth",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(t.p,{children:"Sequence Auth provides a simple way to add account creation, login, session management, and wallet support to your app, enabling applications to provide traditional web experiences (e.g. social login, email login, etc.) while providing access to crypto wallets to interact with smart contracts."}),`
+`,e.jsxs(t.p,{children:["All user and session data is encrypted by a Hardware Security Module operated by a third party. This makes it protected from both external attackers and internal access by Sequence Team or anyone else. ",e.jsx(t.strong,{children:"Nobody"})," can access a user's wallet but the user themselves."]}),`
+`,e.jsxs(t.p,{children:["You won't need to trust us, this is a guarantee you can verify yourself: this service is hosted in a cryptographically attested secure enclave on Amazon Nitro platform. And we offer ",e.jsx(t.a,{href:"https://github.com/0xsequence/waas-authenticator",children:"publicly available independently audited source code"}),", so you can rest assured that it hasn't been tampered with."]}),`
+`,e.jsxs(t.h2,{id:"signer-2-sequence-guard",children:["Signer 2: Sequence Guard",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#signer-2-sequence-guard",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["Sequence Guard is a service hosted by Sequence and contains the other key of the wallets. It's meant to be another line of defense, verifying not only user's ",e.jsx(t.em,{children:"identity"})," but also their ",e.jsx(t.em,{children:"intent"}),". It achieves this in two different ways:"]}),`
+`,e.jsx(t.p,{children:"1 - Limits and allowlists: you can (from the development dashboard) configure what kind of actions are allowed, this means that the guard will only sign transactions within these constraints. For example, you can define that only NFT contract X can be called, and the guard will enforce that no other transactions can take place."}),`
+`,e.jsxs(t.p,{children:["2 - Authenticating the user: the Sequence Guard will authenticate the user ",e.jsx(t.strong,{children:"independently of the Sequence Auth"}),", it performs this action either by using a 3rd party OAuth 2.0 id token (from Meta, Google, X, etc.) or by directly communicating with the user via email/phone number with an OTP code. The direct communication is not always required and, when it does happen, it's seamless and branded with your project details."]}),`
+`,e.jsxs(t.h2,{id:"threat-model",children:["Threat model",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#threat-model",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["With these safeguards in place wallets are ",e.jsx(t.strong,{children:"safe"})," against the following scenarios:"]}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Sequence.app backend is compromised, or guard keys are leaked: in this scenario, the user's identity must still be verified by Sequence Auth."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Sequence Auth is compromised: in this scenario the Sequence Guard protects the wallets, because it independently authenticates the user, an attacker in this scenario wouldn't be able to access any wallets."}),`
+`]}),`
+`]}),`
+`,e.jsx(t.p,{children:"The model also provides partial protection against the scenario:"}),`
+`,e.jsxs(t.ol,{start:"3",children:[`
+`,e.jsx(t.li,{children:"Partner frontend is compromised, or MITM on the partner side: If an attacker were to be able to vulnerate the frontend of the partner, it could gain access to wallets at the time of login, but it cannot gain access to wallets of users who are currently inactive. This means that recovery from such a scenario is possible, with limited damage."}),`
+`]}),`
+`,e.jsxs(t.h2,{id:"waas-api-service",children:["WaaS API Service",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#waas-api-service",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["Sequence WaaS (Wallet as a Service) is an API service that runs on the ",e.jsx(t.a,{href:"https://aws.amazon.com/ec2/nitro/nitro-enclaves/",children:"AWS Nitro Enclaves"})," platform and allows for secure wallet management. It is the technology that powers the ",e.jsx(t.a,{href:"/solutions/wallets/embedded-wallet/overview",children:"Embedded Wallet"}),"."]}),`
+`,e.jsx(t.p,{children:"Wallet private keys are stored encrypted using a split responsibility strategy between Sequence and a trusted third party:"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Sequence is able to access only the encrypted blobs stored in the database with no means of decryption."}),`
+`,e.jsxs(t.li,{children:["A Trusted Third Party owns and operates the ",e.jsx(t.a,{href:"https://aws.amazon.com/kms/",children:"KMS"})," (AWS' hardware security module) used to encrypt the wallet key data. They have no way to export the key material and no access to the encrypted data itself."]}),`
+`]}),`
+`,e.jsx(t.p,{children:"The WaaS Enclave is the only entity able to use the encrypted wallet data. Its security is guaranteed by:"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"The AWS Nitro hypervisor generates and signs a cryptographic attestation based on the actually running enclave code. The document includes a PCR0 (a hash of the enclave code) signed by the Amazon Public Key Infrastructure."}),`
+`,e.jsx(t.li,{children:"The KMS operated by the Trusted Third Party specifies a policy that only allows access to the cryptographic operations if the attestation is valid and the PCR0 hash of the enclave matches the expected value. This means that any - even the smallest - change to the enclave will result in a different attestation and PCR0 hash, effectively invalidating it."}),`
+`,e.jsxs(t.li,{children:["The enclave code is open source and anyone may audit it and verify that the service uses the exact audited version of the source code. ",e.jsx(t.a,{href:"/solutions/wallets/embedded-wallet/implementation-details/enclave-verification",children:"Learn more about verification"}),"."]}),`
+`]})]})}function o(a={}){const{wrapper:t}={...i(),...a.components};return t?e.jsx(t,{...a,children:e.jsx(n,{...a})}):n(a)}export{o as default,r as frontmatter};
---
+++ assets/token-directory-BodrMyGl.js
@@ -0,0 +1,40 @@
+import{d as a,j as e,e as t}from"./index-BG6_g6N7.js";import"./HomePage.css.js.vanilla-l0sNRNKZ.js";const l={title:"Token Directory",description:"undefined"};function s(i){const n={a:"a",code:"code",div:"div",h1:"h1",h2:"h2",h3:"h3",header:"header",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",span:"span",...a(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"token-directory",children:["Token Directory",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#token-directory",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["Sequence maintains an open ",e.jsx(n.a,{href:"https://github.com/0xsequence/token-directory/",children:"token directory on github"}),", that allows Sequence infrastructure to filter spam from verified tokens."]}),`
+`,e.jsxs(n.p,{children:["While the infrastructure also watches sources like ",e.jsx(n.a,{href:"https://www.coingecko.com/",children:"CoinGecko"})," and ",e.jsx(n.a,{href:"https://opensea.com",children:"OpenSea"})," for verified contracts, all new tokens that are to be used - not verified from these sources - must be maintained manually."]}),`
+`,e.jsx(n.p,{children:"When tokens are added to various blockchains, the default is for these tokens to not be included in the default wallet settings."}),`
+`,e.jsx(n.p,{children:"Allowing these tokens to be seen by the wallet and infrastructure (even for testnets) can be done with one of the following steps:"}),`
+`,e.jsxs(n.h2,{id:"how-to-make-tokens-appear-in-stack",children:["How to make tokens appear in stack",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#how-to-make-tokens-appear-in-stack",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/support/token-directory#1-update-wallet-settings",children:"Update Wallet Settings"})," (For Single Wallet): One can update their wallet settings and make a token appear only for a single wallet"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/support/token-directory#2-update-token-directory",children:"Update Token Directory"})," (For All Wallets & Indexer): One can update the token directory to make a token appear for all wallets and Indexer infrastructure"]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"1-update-wallet-settings",children:["1. Update Wallet Settings",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#1-update-wallet-settings",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:["Navigate to ",e.jsx(n.a,{href:"https://sequence.app/settings/general",children:"Settings > General"})," and untoggle ",e.jsx(n.code,{children:"Hide Unverified Tokens"}),"."]}),`
+`,e.jsx(t,{type:"warning",children:e.jsxs(n.p,{children:["Note ",e.jsx("br",{}),`
+This can be useful for testing and immediate token appearance`]})}),`
+`,e.jsxs(n.h3,{id:"2-update-token-directory-recommended",children:["2. Update Token Directory (Recommended)",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#2-update-token-directory-recommended",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:["Create a pull request in the ",e.jsx(n.a,{href:"https://github.com/0xsequence/token-directory/tree/master/index",children:"token directory on github"})," updating the ",e.jsx(n.code,{children:"index"})," folder for the specified network and token type and append to the ",e.jsx(n.code,{children:"tokens"})," array, with updated fields for the specified token:"]}),`
+`,e.jsx(n.pre,{className:"shiki shiki-themes github-light github-dark-dimmed",style:{backgroundColor:"#fff","--shiki-dark-bg":"#22272e",color:"#24292e","--shiki-dark":"#adbac7"},tabIndex:"0",children:e.jsxs(n.code,{children:[e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:"{"})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "chainId": <chain_id>,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "address": "<address>",'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "name": "<name>",'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "type": "<type>,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "symbol": "<SYMBOL>",'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "deployed": true,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "logoURI": "<logo_uri_thumbnail>"'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'      "extensions": {'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'        "link": <token_homepage_url_link>,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'        "description": <description>,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'        "ogImage": "<og_image_banner>",'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'        "originChainId": <origin_chain_id>,'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:'        "originAddress": <origin_address>'})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:"      }"})}),`
+`,e.jsx(n.span,{className:"line",children:e.jsx(n.span,{children:"}"})})]})}),`
+`,e.jsxs(n.h3,{id:"logouri-vs-ogimage",children:[e.jsx(n.code,{children:"logoURI"})," vs ",e.jsx(n.code,{children:"ogImage"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#logouri-vs-ogimage",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"When adding images to describe your token, there are 2 possible options for accomplishing this."}),`
+`,e.jsxs(n.p,{children:["The first is the ",e.jsx(n.code,{children:"logoURI"})," of size 40x40px which represents the rendered thumbnail beside a token in your wallet list"]}),`
+`,e.jsxs(n.p,{children:["The second is the ",e.jsx(n.code,{children:"ogImage"})," of size 1200x640px which represents the rendered banner at the top of a token page when viewing in the wallet."]}),`
+`,e.jsxs(n.h3,{id:"in-wallet",children:["In Wallet",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#in-wallet",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/img/metadata/verified_token_logoURI.png",alt:"Verified Token Metadata"})}),`
+`,e.jsxs(n.h3,{id:"for-a-token",children:["For a Token",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#for-a-token",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/img/metadata/verified_token_metadata.png",alt:"Verified Token Metadata"})})]})}function o(i={}){const{wrapper:n}={...a(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{o as default,l as frontmatter};
