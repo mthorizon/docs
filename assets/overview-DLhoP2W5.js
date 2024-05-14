4ccf230a7f04228da1431f84040cf273a3f331e8
JamesLawton <crossoveranx@gmail.com>
2024-05-14 13:05:13 +0000

    Build: (c4e8b7e) Sequence kit improvements (#116)
    * Update Sequence Kit with Embedded wallet, features.

---
+++ assets/overview-DLhoP2W5.js
@@ -0,0 +1,12 @@
+import{d as s,j as e}from"./index-BvhfKQTF.js";const a={slug:"/unreal-waas-sdk",title:"Introduction",description:"undefined"};function i(r){const n={a:"a",code:"code",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",ul:"ul",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"introduction",children:["Introduction",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#introduction",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["The Sequence Unreal Embedded Wallet SDK provides full Sequence ",e.jsx(n.a,{href:"/solutions/wallets/embedded-wallet/overview",children:"Embedded Wallet"})," and ",e.jsx(n.a,{href:"/api/indexer/overview",children:"Indexer"})," integration for games built on the Unreal Framework."]}),`
+`,e.jsxs(n.p,{children:["This SDK follows ",e.jsx(n.a,{href:"https://semver.org/",children:"Semantic Versioning"})," (",e.jsx(n.code,{children:"major.minor.patch"}),"). While we're still in ",e.jsx(n.code,{children:"0.x.y"})," builds, API breaking changes can be made at any time. After ",e.jsx(n.code,{children:"1.0.0"}),", breaking changes will always cause a ",e.jsx(n.code,{children:"major"})," version increment, non-breaking new features will cause a ",e.jsx(n.code,{children:"minor"})," version increment, and bugfixes will cause a ",e.jsx(n.code,{children:"patch"})," version increment."]}),`
+`,e.jsxs(n.p,{children:["Latest Sequence Unreal SDK ",e.jsx(n.a,{href:"https://github.com/0xsequence/sequence-unreal/releases",children:"releases can be found here"}),"."]}),`
+`,e.jsxs(n.h2,{id:"requirements",children:["Requirements",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#requirements",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Unreal 5.2 or 5.3"}),`
+`,e.jsx(n.li,{children:"Android"}),`
+`,e.jsx(n.li,{children:"iOS"}),`
+`,e.jsx(n.li,{children:"PC standalone"}),`
+`,e.jsx(n.li,{children:"Mac standalone"}),`
+`]})]})}function t(r={}){const{wrapper:n}={...s(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{t as default,a as frontmatter};
