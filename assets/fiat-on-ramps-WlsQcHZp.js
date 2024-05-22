990dd00fc1e0192b0272949b3bb6479877988692
JamesLawton <crossoveranx@gmail.com>
2024-05-22 10:44:58 +0000

    Build: (5f2367d) Update intents.mdx (#90)
    Edited the docs to standardize language: 
    start w/ user perspective (eg tell dev what they can do, not what sequence does); use all direct voice; limit adjectives and adverbs; simple action verbs. 
    
    Standardized formatting.

---
+++ assets/fiat-on-ramps-WlsQcHZp.js
@@ -0,0 +1,12 @@
+import{d as t,j as e}from"./index-Ci3nbDib.js";const a={title:"Fiat On-Ramps",description:"undefined"};function i(r){const n={a:"a",div:"div",h1:"h1",header:"header",img:"img",li:"li",p:"p",ul:"ul",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"fiat-on-ramps",children:["Fiat On-Ramps",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#fiat-on-ramps",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(n.p,{children:"Sequence Wallet allows users to purchase cryptocurrencies directly with their credit card and debit card via on-ramp providers. Currently Sequence supports 6 on-ramp providers;"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.moonpay.com/",children:"Moonpay"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://ramp.network/",children:"Ramp"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.sardine.ai/",children:"Sardine"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://paytrie.com/",children:"PayTrie"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Only providers that support the region the users are in will be displayed."}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/img/fiat-providers.png",alt:"Sequence on-demand sign in"})}),`
+`,e.jsx(n.p,{children:"As a developer integrating the Sequence Wallet, you can choose which payment provider can be visible to users. You can also specify which token will be available to purchase via the on-ramp providers."}),`
+`,e.jsxs(n.p,{children:["To learn more on how to configure the on-ramp options, see ",e.jsx(n.a,{href:"/solutions/wallets/universal-wallet/examples/connect-wallet#wallet-login-and-connect-options",children:"Sequence Connect Options"}),"."]})]})}function o(r={}){const{wrapper:n}={...t(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{o as default,a as frontmatter};
---
+++ assets/indexer-Cy0HWGBZ.js
@@ -0,0 +1,17 @@
+import{d as i,j as e}from"./index-Ci3nbDib.js";import{V as r}from"./Video-B-Glv5kC.js";const s={title:"Indexer in Sequence Builder",description:"undefined"};function t(a){const n={a:"a",div:"div",h1:"h1",h2:"h2",h4:"h4",header:"header",li:"li",p:"p",ul:"ul",...i(),...a.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"indexer-in-sequence-builder",children:["Indexer in Sequence Builder",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#indexer-in-sequence-builder",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(n.p,{children:"Sequence Builder equips game builders with an indexer that collects data from the following networks: Ethereum (and Sepolia), Optimism, Base, Gnosis Chain, Polygon (and Polygon Mumbai), Polygon zkEVM, Arbitrum One, Arbitrum Nova, Gnosis Chain, BNB Smart Chain (and Smart Chain Testnet), Avalanche (and Avalanche Testnet), Oasys Homeverse (and Oasys Homeverse Testnet)."}),`
+`,e.jsxs(n.h4,{id:"what-data-can-i-query-for-my-game",children:["What data can I query for my game?",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#what-data-can-i-query-for-my-game",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"Builder takes the stress out of gathering on-chain data for your game. Simply select the data you want from the indexer, and it generates the necessary code in snippets."}),`
+`,e.jsx(n.p,{children:"Fetch different kinds of data from the indexer:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"tokens and collectibles owned by any wallet"}),`
+`,e.jsx(n.li,{children:"total supply for any token or collectible"}),`
+`,e.jsx(n.li,{children:"the transaction and transfer history for any wallet address"}),`
+`,e.jsx(n.li,{children:"contract metadata for any token or collection"}),`
+`,e.jsx(n.li,{children:"metadata for any collectible"}),`
+`,e.jsx(n.li,{children:"balance change events for any token or collection"}),`
+`]}),`
+`,e.jsxs(n.h4,{id:"how-much-code-will-i-have-to-write-to-query",children:["How much code will I have to write to query?",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#how-much-code-will-i-have-to-write-to-query",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"Sequence Indexer offers several features that level-up your on-chain data capabilities without writing a line of code. It boasts a user-friendly interface that you can use to fetch important data, like the tokens owned by a specific wallet under a specific contract, for instance."}),`
+`,e.jsxs(n.h2,{id:"watch-the-indexer-grab-on-chain-data",children:["Watch the Indexer grab on-chain data",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#watch-the-indexer-grab-on-chain-data",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(r,{videoPreviewSrc:"/img/builder/builder_indexer_preview_video.png",videoSrc:"/video/builder/08_Indexer.mp4"})]})}function c(a={}){const{wrapper:n}={...i(),...a.components};return n?e.jsx(n,{...a,children:e.jsx(t,{...a})}):t(a)}export{c as default,s as frontmatter};
