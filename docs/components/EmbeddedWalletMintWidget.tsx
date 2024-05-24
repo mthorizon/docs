4bb1b0560c72c5a6275cf814b277c99626da0d30
moskalyk <morgan.moskalyk@protonmail.ch>
2024-05-24 16:00:45 -0400

    added widget

--- docs/components/EmbeddedWalletMintWidget.tsx
+++ docs/components/EmbeddedWalletMintWidget.tsx
@@ -9,7 +9,9 @@ import {useSessionHash} from './hooks/useSessionHash.ts'
 
 import embeddedWallet from './EmbeddedWallet/EmbeddedWallet'
 
+
 function EmbeddedWalletMintWidget(props: any) {
+  const indexer = new SequenceIndexer(`https://${props.network}-indexer.sequence.app`, 'AQAAAAAAADi4zbq6FAIlrlW4qD-o_xw0-5A')
 
   const { sessionHash } = useSessionHash()
 
@@ -19,6 +21,49 @@ function EmbeddedWalletMintWidget(props: any) {
   const [isMintedInPresent, setIsMintedInPresent] = useState(false)
   const [alreadyMinted, setAlreadyMinted] = useState(false)
   const [txHash, setTxHash] = useState<any>(null)
+  const [wallet, setWalletAddress] = useState<any>(null)
+
+  useEffect(() => {
+
+    setTimeout(async () => {
+
+    if(wallet){
+
+    // try any contract and account address you'd like :)
+    const contractAddress = '0x3ace164da9ef42843dded5c008a75dcc18d82651'
+
+    // query Sequence Indexer for all nft balances of the account on Polygon
+    const nftBalances = await indexer.getTokenBalances({
+      contractAddress: contractAddress,
+      accountAddress: wallet,
+      includeMetadata: true
+    })
+
+    let isMintedBoolean = false
+
+    nftBalances.balances.map((token) => {
+      if(token.tokenID == '0'){
+        isMintedBoolean = true
+        setAlreadyMinted(true)
+        document.getElementById('mint-button')!.style!.background! = '#3e3e3e'
+        console.log('test')
+      }
+    })
+  }
+
+  }, 0)
+  }, [wallet])
+
+  useEffect(() => {
+    setTimeout(async () => {
+      if(await embeddedWallet.isSignedIn()){
+        setWalletAddress(await embeddedWallet.getAddress())
+      }
+    }, 0)
+  }, [])
+  useEffect(() => {
+
+  }, [alreadyMinted])
 
   const handleGoogleLogin = async (tokenResponse: CredentialResponse) => {
     console.log(tokenResponse)
@@ -27,24 +72,18 @@ function EmbeddedWalletMintWidget(props: any) {
       idToken: tokenResponse.credential!
     }, "Sequence Docs")
     console.log(res)
+    setWalletAddress(res.wallet)
   }
 
   const handleButtonClick = async () => {
-    try {
-      const wallet = await sequence.getWallet()
-      const details = await wallet.connect({app: 'Sequence Docs'})
-  
-      if(details.connected){
-        const indexer = new SequenceIndexer(`https://${props.network}-indexer.sequence.app`, 'AQAAAAAAADi4zbq6FAIlrlW4qD-o_xw0-5A')
 
         // try any contract and account address you'd like :)
-        const contractAddress = '0x58eb15e3b19545b5c01cbd38dac7497ef924b168'
-        const accountAddress = details.session?.accountAddress
+        const contractAddress = '0x3ace164da9ef42843dded5c008a75dcc18d82651'
 
         // query Sequence Indexer for all nft balances of the account on Polygon
         const nftBalances = await indexer.getTokenBalances({
           contractAddress: contractAddress,
-          accountAddress: accountAddress,
+          accountAddress: wallet,
           includeMetadata: true
         })
 
@@ -64,7 +103,7 @@ function EmbeddedWalletMintWidget(props: any) {
         } else {
           const url = 'https://docs-collectible.tpin.workers.dev';
           const data = {
-            address: details.session?.accountAddress
+            address: wallet
           };
           const res = await fetch(url, {
             method: 'POST',
@@ -74,24 +113,22 @@ function EmbeddedWalletMintWidget(props: any) {
             body: JSON.stringify(data),
           })
           setTxHash(await res.text())
-          setIsMintedInPresent(true)
+          setAlreadyMinted(true)
+          setIsMinting(false)
           document.getElementById('mint-button')!.style!.background! = '#3e3e3e'
         }
-      }
-    }catch(err){
-      console.log('user closed the wallet')
-    }
   };
 
-  const openWallet = () => {
-    console.log('opening wallet')
-    const wallet = sequence.getWallet()
-    const path = "wallet/collectibles/421614/0x58eB15E3B19545B5c01Cbd38Dac7497ef924B168/0";
-    wallet.openWallet(path);
-  }
   const signOut = async () => {
     const sessions = await embeddedWallet.listSessions()
-    await embeddedWallet.dropSession({ sessionId: sessions[0].id })
+    for(let i = 0; i < sessions.length; i++){
+      await embeddedWallet.dropSession({ sessionId: sessions[i].id })
+    }
+    setWalletAddress(null)
+    setAlreadyMinted(false)
+    setIsMinting(false)
+    setIsMintedInPresent(false)
+    setTxHash(null)
   }
   return (
     <div className="widget">
@@ -99,26 +136,36 @@ function EmbeddedWalletMintWidget(props: any) {
       <div className="widget-header">
       </div>
       <div className="widget-body">
-
-        <img className='mint-image' src={'https://media.discordapp.net/attachments/1091126083895693312/1220699875637985360/morganmoskalyk_a_style_of_this_53c1a28d-df6c-4500-bdb9-1f9b6cec5c97.png?ex=660fe459&is=65fd6f59&hm=a7a871f3fe3245149f07b76e96b5aa2a05754add5d305f311e164f050d21f15c&=&format=webp&quality=lossless&width=560&height=560'}/>
+        <img className='mint-image' src={'https://metadata.sequence.app/projects/15984/collections/232/tokens/0/image.png'}/>
         <br/>
-        <div style={{float:'left'}}>
-            <button className='mint-button' id="mint-button" disabled={isMinting || txHash != null} onClick={handleButtonClick}>{isMinting ? isMintedInPresent ? alreadyMinted ? "already minted ✓" : "minted ✓" : 'minting...' : 'collect'}</button>
-            { isMintedInPresent || alreadyMinted ? <button className='mint-button' id="mint-button" onClick={() => openWallet()}>{'open wallet'}</button> : null }
+        <div style={{ display: 'flex', alignItems: 'center', float:'left'}}>
+            {wallet && <button className='mint-button' id="mint-button" disabled={alreadyMinted || txHash != null} onClick={handleButtonClick}>{
+              alreadyMinted 
+            ? 
+              !isMinting 
+            ? 
+              "Minted ✓" 
+            : 
+              'Minting...' 
+            : 
+              'Collect'
+            }</button>}
+            {wallet && <div  className='mint-button' id="mint-button" onClick={() => signOut()}>Sign Out</div>}
         </div>
-        <button className='mint-button' id="mint-button" disabled={isMinting || txHash != null} onClick={signOut}>sign out</button>
-        <GoogleLogin 
-           nonce={sessionHash}
-           key={sessionHash}
-           onSuccess={handleGoogleLogin} shape="circle" width={230} />
-      
+        {wallet == null && <div  className='mint-button' id="mint-button">Gmail Sign In
+         <div style={{overflow: 'hidden', opacity: '0', marginTop: '-35px',width: '90px', position: 'absolute', zIndex: 1, height: '100px'}}>
+          <GoogleLogin 
+          nonce={sessionHash}
+          key={sessionHash}
+          onSuccess={handleGoogleLogin} shape="circle" width={230} />
+        </div> 
+        </div> }
       </div> 
       <div className="widget-footer">
         <br/>
-        {isMintedInPresent && txHash ? <div className="dashed-box">
-          <a href={`https://sepolia.arbiscan.io/tx/${txHash}`} target="_blank">{`arbiscan.io: ${txHash.slice(0,8)}`}...</a>
+        {txHash ? <div className="dashed-box">
+          <a href={`https://testnet-explorer-v2.xai-chain.net/${txHash}`} target="_blank">{`arbiscan.io: ${txHash.slice(0,8)}`}...</a>
         </div> : <p>Create a wallet and mint a free collectible on {props.network.replace('-', ' ')} in seconds</p>}
-      
       </div>
 		</GoogleOAuthProvider>
     </div>
