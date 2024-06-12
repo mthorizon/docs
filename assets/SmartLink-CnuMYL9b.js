864c7ef2a7c58137a684b098b5cb6669d96ef7b8
JamesLawton <crossoveranx@gmail.com>
2024-06-12 15:19:45 +0000

    Build: (94a38d7) updated template language and fixed broken linking (#167)

---
+++ assets/SmartLink-CnuMYL9b.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-Dm3B_kmQ.js";const s=({href:t,...r})=>t!=null&&t.startsWith("/")?n.jsx("a",{href:t,...r}):n.jsx("a",{href:t,target:"_blank",rel:"noopener noreferrer",...r});export{s as S};
