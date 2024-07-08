d1dc2b08c1a3ff5b79bb90c75dde9e19fa0ff460
moskalyk <morgan.moskalyk@protonmail.ch>
2024-07-08 18:16:40 +0000

    Build: (cf54063) Update embedded wallet jwt parsing (#207)
    * updated jwt id_token parsing contents from apple developer docs
    
    * updated definition

---
+++ assets/installation-Y7lAAp8f.js
@@ -0,0 +1,16 @@
+import{d as r,j as e}from"./index-BnRlAZSq.js";const o={title:"Sequence - Web3 Gaming Platform - Unreal SDK Installation",description:"Documentation for Unreal SDK Installation for the Sequence infrastructure stack for web3 gaming."};function i(t){const n={a:"a",code:"code",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",ol:"ol",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"installation",children:["Installation",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#installation",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Once you have the ",e.jsx(n.code,{children:"SequencePlugin"})," folder, you'll need to go to your project directory and create a ",e.jsx(n.code,{children:"Plugins"}),` folder in it,
+then copy over the `,e.jsx(n.code,{children:"SequencePlugin"})," folder into the ",e.jsx(n.code,{children:"Plugins"})," folder. If a ",e.jsx(n.code,{children:"Plugins"})," folder already exists just copy the ",e.jsx(n.code,{children:"SequencePlugin"})," folder into it."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Launch your project, then allow it to update the UProject Settings."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["To find the ",e.jsx(n.code,{children:"SequencePlugin"})," content folder in your content drawer enable view plugin content"]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"using-epic-games-marketplace",children:["Using Epic Games marketplace",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#using-epic-games-marketplace",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"To get started with the Sequence SDK for Unreal, follow the link below."}),`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.unrealengine.com/marketplace/en-US/product/sequence-embedded-wallets",children:"https://www.unrealengine.com/marketplace/en-US/product/sequence-embedded-wallets"})})]})}function c(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{c as default,o as frontmatter};
