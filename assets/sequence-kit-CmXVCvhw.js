d496281eeb64638610453607030ee24dc904ed9f
JamesLawton <crossoveranx@gmail.com>
2024-07-10 10:05:52 +0000

    Build: (667539a) Transaction manager quickstart changes (#211)
    * updated quickstart
    
    * updated quickstart guide for transactions manager

---
+++ assets/sequence-kit-CmXVCvhw.js
@@ -0,0 +1,17 @@
+import{d as s,j as e}from"./index-DaB9FR7r.js";const a={slug:"overview",title:"Sequence Kit Documentation",description:"undefined"};function i(n){const t={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"sequence-kit-seamlessly-integrate-web3-wallets-into-your-applications",children:["Sequence Kit: Seamlessly Integrate Web3 Wallets into Your Applications",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#sequence-kit-seamlessly-integrate-web3-wallets-into-your-applications",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(t.p,{children:["Sequence Kit 🧰 is the ultimate toolkit for effortlessly integrating web3 wallets into your applications, providing your users with a smooth and secure onboarding experience. With our robust tools built on the popular ",e.jsx(t.a,{href:"https://wagmi.sh/",children:"wagmi"})," library, unlock a realm of possibilities in the world of web3."]}),`
+`,e.jsxs(t.h2,{id:"key-features",children:["Key Features",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#key-features",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Universal Connections"}),": Seamlessly connect via popular social logins such as Facebook, Google, Discord, and more! Your users will enjoy a smooth, secure onboarding process. 🔐🪪"]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Web3 Wallet Integration"}),": Effortlessly integrate with leading web3 wallets like WalletConnect and MetaMask. Unleash the power of blockchain with just a few clicks! 🦊 ⛓️"]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Embedded Wallet Experience"}),": Provide your users with a comprehensive in-game wallet, enabling them to manage their coins and collectibles all within your own application. 👛 🖼️ 🪙"]}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.p,{children:["Explore the potential of Sequence Kit by trying out our ",e.jsx(t.a,{href:"https://0xsequence.github.io/kit",children:"demo"}),"!"]}),`
+`,e.jsx(t.header,{children:e.jsxs(t.h1,{id:"next-steps",children:["Next Steps",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#next-steps",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(t.p,{children:["Ready to integrate Sequence Kit into your application? Check out our ",e.jsx(t.a,{href:"/solutions/wallets/sequence-kit/getting-started",children:"Getting Started guide"}),". If you're already familiar with Sequence Kit dive into the available ",e.jsx(t.a,{href:"/solutions/wallets/sequence-kit/configuration",children:"configuration options"})," or take a look at the ",e.jsx(t.a,{href:"/solutions/wallets/sequence-kit/checkout",children:"checkout"}),"."]})]})}function r(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{r as default,a as frontmatter};
