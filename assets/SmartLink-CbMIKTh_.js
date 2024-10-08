44f4253cc1a908c3dfcf1edc4c9b7a4523059744
JamesLawton <crossoveranx@gmail.com>
2024-10-08 12:05:33 +0000

    Build: (6d01b9f) updated build flow, separated search-index weekly cadence (#328)

---
+++ assets/SmartLink-CbMIKTh_.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-8GJ9zA5g.js";const s=({href:t,...r})=>t!=null&&t.startsWith("/")?n.jsx("a",{href:t,...r}):n.jsx("a",{href:t,target:"_blank",rel:"noopener noreferrer",...r});export{s as S};
