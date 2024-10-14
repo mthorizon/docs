d372cd2bd35022a644baf469979f37fc92ef3cd3
LukasJenicek <lukas.jenicek5@gmail.com>
2024-10-14 08:43:42 +0000

    Build: (2a6489b) enable multiple examples per request/response plus define summary (#352)

---
+++ assets/SmartLink-Dgz63Wm3.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-Bao9fo3L.js";const s=({href:t,...r})=>t!=null&&t.startsWith("/")?n.jsx("a",{href:t,...r}):n.jsx("a",{href:t,target:"_blank",rel:"noopener noreferrer",...r});export{s as S};
