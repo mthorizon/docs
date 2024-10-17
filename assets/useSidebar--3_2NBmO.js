ac45b79ad04fd5ea3d68b39891b336b16704fe6e
JamesLawton <crossoveranx@gmail.com>
2024-10-17 11:13:29 +0000

    Build: (94c06be) [AUTOMATED] Update: proto/docs/indexer.gen.yaml (#365)
    Co-authored-by: VojtechVitek <139342+VojtechVitek@users.noreply.github.com>
    Co-authored-by: James Lawton <crossoveranx@gmail.com>

---
+++ assets/useSidebar--3_2NBmO.js
@@ -0,0 +1,1 @@
+import{r as a}from"./APIComponent-C0Y6oxYO.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
