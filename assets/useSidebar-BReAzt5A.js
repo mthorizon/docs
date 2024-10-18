553bcd8e3794459c9feaf94a89a9343037e063aa
JamesLawton <crossoveranx@gmail.com>
2024-10-18 14:33:48 +0000

    Build: (e2409ad) Scalar vh (#375)
    * fixed styles
    
    * added child css targeting

---
+++ assets/useSidebar-BReAzt5A.js
@@ -0,0 +1,1 @@
+import{r as a}from"./APIComponent-C1uM_Jho.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
--- assets/useSidebar-BTkffWs9.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./APIComponent-C2n6o9em.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
