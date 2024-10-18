6026caedee572d234ee6c31781ac4e4f726c7fbc
JamesLawton <crossoveranx@gmail.com>
2024-10-18 10:54:47 +0000

    Build: (72db338) fixed scrollbar and window reload (#372)

---
+++ assets/useSidebar-DhE_v3G8.js
@@ -0,0 +1,1 @@
+import{r as a}from"./APIComponent-BZ0l9kev.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
