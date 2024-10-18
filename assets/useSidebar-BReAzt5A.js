6158af6447b2dfe53aad65866eaf3697815b0b63
JamesLawton <crossoveranx@gmail.com>
2024-10-18 15:07:36 +0000

    Build: (798f2e9) Scalar vh (#376)
    * fixed styles
    
    * added child css targeting
    
    * specified child css class

--- assets/useSidebar-BReAzt5A.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./APIComponent-C1uM_Jho.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
