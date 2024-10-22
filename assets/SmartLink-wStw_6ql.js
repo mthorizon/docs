0d6dd065aa2d17580a87d679b90cc0badc008f11
JamesLawton <crossoveranx@gmail.com>
2024-10-22 13:59:13 +0000

    Build: (03b169f) Guides/unreal primary sales (#382)
    * started adding primary sale guide for unreal
    
    * fixed missing bracket
    
    ---------
    
    Co-authored-by: Andy Grüning <andygruening@gmail.com>

---
+++ assets/SmartLink-wStw_6ql.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-C81m1MD-.js";const s=({href:t,...r})=>t!=null&&t.startsWith("/")?n.jsx("a",{href:t,...r}):n.jsx("a",{href:t,target:"_blank",rel:"noopener noreferrer",...r});export{s as S};
--- assets/useSidebar-DTSG7v9n.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./APIComponent-B0ZkMz7z.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
