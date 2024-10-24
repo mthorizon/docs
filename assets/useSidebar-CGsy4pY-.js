3a6126978f86d2e49742d1de5a123b414c099205
JamesLawton <crossoveranx@gmail.com>
2024-10-24 12:38:59 +0000

    Build: (3c3c672) [AUTOMATED] Update Stack/API openapi docs (#394)
    * [AUTOMATED] Update: api/proto/docs/api.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * [AUTOMATED] Update: api/proto/docs/api.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    ---------
    
    Co-authored-by: LukasJenicek <6400599+LukasJenicek@users.noreply.github.com>

--- assets/useSidebar-CGsy4pY-.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./APIComponent-D4yCWLd1.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
