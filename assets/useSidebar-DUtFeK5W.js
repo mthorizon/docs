7e1faed4c0f789eac850a6eed4c19f60bdb26c72
JamesLawton <crossoveranx@gmail.com>
2024-10-25 12:03:59 +0000

    Build: (79dc6b6) Metadata endpoints updt r (#397)
    * Add more information to the interactive metadata API in the docs
    
    * Modify metadata API examples.json and metadata.gen.yaml with more information
    
    * updated parser for Metadata
    
    * reverted to master metadata gen
    
    * Update examples.json
    
    * changed language for metadata
    
    * fixed chainId for example
    
    ---------
    
    Co-authored-by: Richard Miguel Irala <richardirala47@hotmail.com>

---
+++ assets/useSidebar-DUtFeK5W.js
@@ -0,0 +1,1 @@
+import{bg as a}from"./APIComponent-B2mFmuI6.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
