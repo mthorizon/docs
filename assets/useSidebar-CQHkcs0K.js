2c5f9b2ae0d89287a15bffc9144f6ecc752a923e
JamesLawton <crossoveranx@gmail.com>
2024-10-25 11:08:55 +0000

    Build: (aecac50) Stack api (#396)
    * added stack pages
    
    * updated parsing and added transactions
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    ---------
    
    Co-authored-by: JamesLawton <19821370+JamesLawton@users.noreply.github.com>
    Co-authored-by: 0xsequence-bot <50996415+0xsequence-bot@users.noreply.github.com>

---
+++ assets/useSidebar-CQHkcs0K.js
@@ -0,0 +1,1 @@
+import{bg as a}from"./APIComponent-CJmCIR2C.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
