af38e831cac71e60cf2a7016c88a1e8bdaeeac1b
LukasJenicek <lukas.jenicek5@gmail.com>
2024-10-29 12:20:38 +0000

    Build: (2002a64) [AUTOMATED] Update relayer openapi docs (#405)
    * [AUTOMATED] Update: relayer/proto/docs/relayer.gen.yaml
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    ---------
    
    Co-authored-by: pkieltyka <18831+pkieltyka@users.noreply.github.com>

---
+++ assets/useSidebar-CxU2Py08.js
@@ -0,0 +1,1 @@
+import{bj as a}from"./APIComponent-CWqMnc8R.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
