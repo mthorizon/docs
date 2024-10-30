e92d71176a00c3ebffaeeaa55620c658cdfc9116
JamesLawton <crossoveranx@gmail.com>
2024-10-30 16:06:37 +0000

    Build: (679d7e6) [AUTOMATED] Update files from 0xsequence/builder (#417)
    * [AUTOMATED] Update: api/proto/docs/analytics.gen.yaml
    
    * add global security scheme
    
    * [AUTOMATED] Update: docs/pages/api/**/*.gen.yaml
    
    * import analytics instead of indexer
    
    ---------
    
    Co-authored-by: LukasJenicek <6400599+LukasJenicek@users.noreply.github.com>
    Co-authored-by: Lukas Jenicek <lj@horizon.io>

---
+++ assets/useSidebar-vXXNkGO-.js
@@ -0,0 +1,1 @@
+import{bg as a}from"./APIComponent-DRFOyQCd.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
