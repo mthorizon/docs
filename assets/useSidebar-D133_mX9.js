c10ec28b47664416e501771f2616f125790470e9
JamesLawton <crossoveranx@gmail.com>
2024-11-06 11:53:59 +0000

    Build: (2f741f7) [AUTOMATED] Update: api/proto/docs/builder.gen.yaml (#431)
    Co-authored-by: LukasJenicek <6400599+LukasJenicek@users.noreply.github.com>

--- assets/useSidebar-D133_mX9.js
+++
@@ -1,1 +0,0 @@
-import{bh as a}from"./APIComponent-Bda6ZYMS.js";const o=a({}),d=(e,r)=>o[e]=r,l=e=>{o[e]=!o[e]},t=()=>({collapsedSidebarFolders:o,setCollapsedSidebarFolder:d,toggleSidebarFolder:l});export{t as a};
